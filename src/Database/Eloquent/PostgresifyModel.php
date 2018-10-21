1c8202079e16f995c85178027fd9ba3ea53ed612
Marti Markov <marti.markov@icloud.com>
2018-10-21 17:42:03 +0100

    Added auto convertion of _from & _to when using Range types.

--- src/Database/Eloquent/PostgresifyModel.php
+++ src/Database/Eloquent/PostgresifyModel.php
@@ -11,7 +11,42 @@ use Smiarowski\Postgres\Model\Traits\PostgresArray;
 
 class PostgresifyModel extends Model
 {
+
     use PostgresArray;
+
+    public function __construct(array $attributes = [])
+    {
+        $casts = $this->getCasts();
+
+        $rangeCasts = [];
+        foreach ($casts as $cast => $type) {
+            if (strtolower($type) === 'numericrange' || strtolower($type) === 'integerrange') {
+                $rangeCasts[$cast] = $type;
+            }
+        }
+
+        foreach ($rangeCasts as $attribute => $attributeCast) {
+            $fromAttributeName = $attribute . '_from';
+            $toAttributeName = $attribute . '_to';
+            if (array_key_exists($fromAttributeName, $attributes) and array_key_exists($toAttributeName, $attributes)) {
+                $from = $attributes[$attribute . '_from'];
+                $to = $attributes[$attribute . '_to'];
+                switch (strtolower($attributeCast)) {
+                    case 'numericrange':
+                        $attributes[$attribute] = new NumericRange($from, $to);
+                        unset($attributes[$attribute . '_from']);
+                        unset($attributes[$attribute . '_to']);
+                        break;
+                    case 'integerrange':
+                        $attributes[$attribute] = new IntegerRange($from, $to);
+                        unset($attributes[$attribute . '_from']);
+                        unset($attributes[$attribute . '_to']);
+                        break;
+                }
+            }
+        }
+        parent::__construct($attributes);
+    }
 
     public function setAttribute($key, $value)
     {
@@ -23,6 +58,7 @@ class PostgresifyModel extends Model
                     break;
             }
         }
+
         return parent::setAttribute($key, $value);
     }
 
@@ -36,10 +72,19 @@ class PostgresifyModel extends Model
             case 'array':
                 return self::accessPgArray($value);
             case 'numericrange':
+                if (is_a($value, NumericRange::class)) {
+                    return $value;
+                }
                 return (new NumericRange(0, 0))->fromPgValues($value);
             case 'timestamprange':
+                if (is_a($value, TimestampRange::class)) {
+                    return $value;
+                }
                 return (new TimestampRange(0, 0))->fromPgValues($value);
             case 'integerrange':
+                if (is_a($value, IntegerRange::class)) {
+                    return $value;
+                }
                 return (new IntegerRange(0, 0))->fromPgValues($value);
         }
 
@@ -54,7 +99,11 @@ class PostgresifyModel extends Model
      */
     protected function isJsonCastable($key)
     {
-        return $this->hasCast($key, ['json', 'object', 'collection']);
+        return $this->hasCast($key, [
+            'json',
+            'object',
+            'collection',
+        ]);
     }
 }
 
